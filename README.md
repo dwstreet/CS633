9c10cfb5bb4fa7749046fce6848471f85ff9c077
Dan Street <dw01stre@alum.siena.edu>
2019-02-09 17:22:42 -0500

    updated readme, still tinkering on restaurant;

--- README.md
+++ README.md
@@ -1,4 +1,40 @@
 # CS633
 Reservation System
 
-This will be the readme for the reservation system.  I may add markdown tags to this file if I end up adding anything useful to this README.
+I'm using Eclipse and have provided some links for setting up your own workspace.
+
+Setup instructions (Eclipse):
+
+1) Follow instructions at this link https://download.java.net/general/javafx/eclipse/tutorial.html
+2) Import this git project into your workspace
+3) If your project's eclipse natures are incorrect then copy the xml underneath into your .project file (auto created after you import the project to your workspace)
+4) Ensure that you are using Jdk8 or greater (you can use this link for more information -- https://www.gamefromscratch.com/post/2011/11/15/Telling-Eclipse-to-use-the-JDK-instead-of-JRE.aspx)
+5) Ensure that your project is actually using the JavaFx library in the BuildPath
+
+-----------------------------------
+MY .project CONTENTS
+-----------------------------------
+
+<?xml version="1.0" encoding="UTF-8"?>
+<projectDescription>
+	<name>asd</name>
+	<comment></comment>
+	<projects>
+	</projects>
+	<buildSpec>
+		<buildCommand>
+			<name>org.eclipse.jdt.core.javabuilder</name>
+			<arguments>
+			</arguments>
+		</buildCommand>
+		<buildCommand>
+			<name>org.eclipse.xtext.ui.shared.xtextBuilder</name>
+			<arguments>
+			</arguments>
+		</buildCommand>
+	</buildSpec>
+	<natures>
+		<nature>org.eclipse.xtext.ui.shared.xtextNature</nature>
+		<nature>org.eclipse.jdt.core.javanature</nature>
+	</natures>
+</projectDescription>
--- src/application/Restaurant.java
+++ src/application/Restaurant.java
@@ -1,17 +1,85 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Map;
+
 public class Restaurant {
 
 	private String name;
-	private int floatingSeats;
-	private int stableSeats;
+	private String phone;
+	private Map<Date, WorkingDay> schedule;
 	
-	public Restaurant(String name, int floatingSeats, int stableSeats) {
+	
+	public Restaurant(String name, String phone, int floatingSeats, int stableSeats, Map<Date, WorkingDay> schedule) {
 		
 		this.name = name;
-		this.floatingSeats = floatingSeats;
-		this.stableSeats = stableSeats;
+		this.phone = phone;
+		this.schedule = schedule;
+	}
+		
+	public String getName() {
+		return name;
 	}
 	
+	public String getPhone() {
+		return phone;
+	}
 	
+	public Map<Date, WorkingDay> getSchedule(){
+		return schedule;
+	}
+	
+	public Reservation makeReservation() {
+		
+		return new Reservation(0, 0, 0);
+	}
+	
+	private class Reservation {
+		
+		private WorkingDay date;
+		private int floatingSeats;
+		private int stableSeats;
+		
+		Reservation(int year, int month, int day) {
+			
+			
+			
+		}
+		
+		public int getFloatingSeats() {
+			return floatingSeats;
+		}
+		
+		public int getStableSeats() {
+			return stableSeats;
+		}
+		
+		public boolean canBook(int partyOf) {
+			
+			return partyOf < (floatingSeats + stableSeats);
+		}
+		
+		public void bookParty(Reservation resv, int partyOf) {
+			
+		}
+	}
+	
+	private static class WorkingDay {
+		
+		private ArrayList<Reservation> reservations;
+		
+		@SuppressWarnings("deprecation")
+		private WorkingDay() {
+			reservations = new ArrayList<Reservation>();
+		}
+		
+		static ArrayList<Reservation> getReservations(int year, int month, int day) {
+			
+				
+			
+			
+			return null;
+		}
+	}
 }
